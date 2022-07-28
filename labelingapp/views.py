c2d966b7f880055c1495f58332d7aa1a2c310faa
gabang2 <offbeat1020@naver.com>
2022-07-28 13:45:15 +0900

    새 라벨 안들어가는 오루 & 다른 카테고리의 auto labeling 오류 해결

--- labelingapp/views.py
+++ labelingapp/views.py
@@ -81,7 +81,7 @@ def labeling_work(request):
                 # 자동 라벨링 - 검색
                 review_first = print_review(start, end, category_product)
                 current_review = review_first[0].review_content
-                compare_data = FirstLabeledData.objects.all()
+                compare_data = FirstLabeledData.objects.filter(category_id__in=category_detail)
                 auto_data_id = []
                 for i in compare_data:
                     if current_review.__contains__(i.first_labeled_target) and current_review.__contains__(
@@ -136,7 +136,7 @@ def labeling_work(request):
                     print(target, emotion, expression)
                     if not FirstLabeledData.objects.filter(first_labeled_emotion=emotion, first_labeled_target=target,
                                                            first_labeled_expression=expression,
-                                                           category_id=category_id):
+                                                           category_id=category_id, review_id=review_id):
                         # First_Labeled_Data모델을 불러와서 first_labeled_data에 저장
                         first_labeled_data = FirstLabeledData()
 
@@ -162,7 +162,7 @@ def labeling_work(request):
                     ######---- 자동 라벨링 ----#####
                     # 자동라벨링 - 검색
                     current_review = review_first[0].review_content
-                    compare_data = FirstLabeledData.objects.all()
+                    compare_data = FirstLabeledData.objects.filter(category_id__in=category_detail)
                     auto_data_id = []
                     for i in compare_data:
                         if current_review.__contains__(i.first_labeled_target) and current_review.__contains__(
